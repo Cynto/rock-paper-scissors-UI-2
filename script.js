bbf04e5468b7a0e0f2f448b1bc495c1780038198
Cynto <lucagaravello@outlook.com>
2021-01-19 20:58:44 +0100

    Finished most of logic, edited ui a little

--- script.js
+++ script.js
@@ -1,41 +1,128 @@
 let computerScore = 0;
 let playerScore = 0;
-let round = 0;
+let round = 1;
 
 //Declaring variables which will be updated during match
 const playerScoreText = document.querySelector('#playerScore');
 const computerScoreText = document.querySelector('#computerScore');
+const roundText = document.querySelector('#round');
+const playerChoice = document.querySelector('#playerChoice');
+const computerChoice = document.querySelector('#computerChoice');
+const container = document.querySelector('#container');
+const end = document.querySelector('#end');
+
+
 
 //Buttons which event listners will be added to
 const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
 const scissorsButton = document.querySelector('#scissors');
+const resetButton = document.querySelector('#reset')
 
-
-function computerPlay() { //Function randomly returns rock, paper or scissors
-    let computerOptions = ["Rock", "Paper", "Scissors"];
+//Function randomly returns rock, paper or scissors
+function computerPlay() { 
+    let computerOptions = ["rock", "paper", "scissors"];
 
     let computerChosen = computerOptions[Math.floor(Math.random()*computerOptions.length)] //randomly picks an element from the array
     return computerChosen;
 
 }
-function playRound(computerSelection, playerSelection) { //This function checks whether the player or computer had the winning move
-    playerSelection = playerSelection.toLowerCase(); //changes player's input to lowercase, so it doesn't matter whether or not the player entered a lower or higher case word
-    console.log(`You chose ${playerSelection}.`)
-    console.log(`The computer chose ${computerSelection}.`);
+roundText.textContent = `Round: ${round}`;
+//function uses 
+function playGame(playerSelection, computerSelection) {
     
-    if(computerSelection == "rock" && playerSelection == "paper" ||
-        computerSelection == "scissors" && playerSelection == "rock" ||
-        computerSelection == "paper" && playerSelection == "scissors") { 
-        return "win" 
+    
+    
+    if(round < 5) {
+        round++;
+        roundText.textContent = `Round: ${round}`; 
+        
+        computerSelection = computerPlay();
+       
+        playerChoice.setAttribute('style', 'color: #d1caca')
+        computerChoice.setAttribute('style', 'color: #d1caca')
+        playerChoice.textContent = `Player choice: ${playerSelection}`;
+        computerChoice.textContent = `Computer choice: ${computerSelection}`;
+        
+        if(computerSelection == 'rock' && playerSelection == 'paper' ||
+            computerSelection == 'scissors' && playerSelection == 'rock' ||
+            computerSelection == 'paper' && playerSelection == 'scissors') { 
+            
+                playerScore++;
+                
+                playerScoreText.textContent = `You: ${playerScore}`;
+                computerScoreText.textContent = `Computer: ${computerScore}`;
+                playerChoice.setAttribute('style', 'color: red')
+                
+                
+
+        }
+        else if(computerSelection == playerSelection){ 
+            computerScore++;
+            playerScore++;
+            playerScoreText.textContent = `You: ${playerScore}`;
+            computerScoreText.textContent = `Computer: ${computerScore}`;
+        
+            
+        }
+        else {
+            computerScore++;
+            playerScoreText.textContent = `You: ${playerScore}`;
+            computerScoreText.textContent = `Computer: ${computerScore}`;
+            computerChoice.setAttribute('style', 'color: red')
+            
     }
-    else if(computerSelection == playerSelection){ 
-        return "tie"
+    
+        
     }
     else {
-        return "lose"
-    }
-    
-    
+        if(playerScore > computerScore) {
+            playerScoreText.setAttribute('style', 'color: red;');
+            
+            end.textContent = 'You beat the computer!';
+            
+        }
+        else if(computerScore > playerScore) {
+            computerScoreText.setAttribute('style', 'color: red;');
+            
+            end.textContent = 'You lost!';
+            
+        }
+        else {
+            playerScoreText.setAttribute('style', 'color: red;');
+            computerScoreText.setAttribute('style', 'color: red;')
+            
+           
+            
+            end.textContent = 'It\'s a tie!';
+            
+        }
+        resetButton.style.display = 'block';
+        resetButton.textContent = 'Reset';
+        
+        
+        
+        //reset button refreshes site 
+        resetButton.addEventListener('click', () => {
+            window.location.reload(true);
+        })
 
+    }
 }
+
+
+
+//event listener added to buttons
+rockButton.addEventListener('click', () => {
+    playGame('rock');
+});
+paperButton.addEventListener('click', () => {
+    playGame('paper');
+});
+scissorsButton.addEventListener('click', () => {
+    playGame('scissors');
+});
+
+
+
+
